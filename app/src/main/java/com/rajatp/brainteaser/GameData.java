77aa07bddba6f272a00a598296c95c7b2983b9a1
rajatp1995 <you@example.com>
2019-04-08 20:34:57 +0530

    Added Firebase

---
+++ app/src/main/java/com/rajatp/brainteaser/GameData.java
@@ -0,0 +1,79 @@
+package com.rajatp.brainteaser;
+
+public class GameData {
+    int id;
+    String ques;
+    String op1;
+    String op2;
+    String op3;
+    String op4;
+    String userAns;
+    String correctAns;
+
+    public GameData() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+
+    public String getOp1() {
+        return op1;
+    }
+
+    public void setOp1(String op1) {
+        this.op1 = op1;
+    }
+
+    public String getOp2() {
+        return op2;
+    }
+
+    public void setOp2(String op2) {
+        this.op2 = op2;
+    }
+
+    public String getOp3() {
+        return op3;
+    }
+
+    public void setOp3(String op3) {
+        this.op3 = op3;
+    }
+
+    public String getOp4() {
+        return op4;
+    }
+
+    public void setOp4(String op4) {
+        this.op4 = op4;
+    }
+
+    public String getUserAns() {
+        return userAns;
+    }
+
+    public void setUserAns(String userAns) {
+        this.userAns = userAns;
+    }
+
+    public String getCorrectAns() {
+        return correctAns;
+    }
+
+    public void setCorrectAns(String correctAns) {
+        this.correctAns = correctAns;
+    }
+}
